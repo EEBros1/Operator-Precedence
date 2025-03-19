38ac095354826abbe012b954f6242c3dcdca7e56
EEBros1 <edebo4ican@gmail.com>
2025-03-19 17:37:37 +0000

    Add files via upload

---
+++ Operator Precedence.py
@@ -0,0 +1,16 @@
+V = 81
+W = 98
+X = 23
+Y = 56
+Z = 0
+Z = (V + W) * X // Y ;
+print ("The value of (V + W) * X // Y is", Z)
+
+#New topic of code
+
+Name = input ("Say your username")
+Password = input ("Enter your password")
+if Name == ("Asphalt Legend") and Password == ("AsphaltGamer239") :
+    print ("Access aprroved")
+else :
+    print ("Access denied")
